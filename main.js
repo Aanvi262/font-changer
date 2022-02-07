300853bb33c636f039b96fba61e90414a7b73b65
Aanvi262 <73943879+Aanvi262@users.noreply.github.com>
2022-02-07 16:33:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
+
+function setup(){
+video = createCapture(VIDEO);
+video.size(550,500);
+canvas = createCanvas(550,550);
+canvas.position = (560,150);
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log('poseNet is initialized!');
+}
+
+function gotPoses(results){
+if(results.length > 0){
+    console.log(results);
+
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x;
+    difference = floor(leftWristX - rightWristX);
+    console.log("leftWristX = "+ leftWristX + "rightWristX =" + rightWristX + "difference = " + difference);
+}
+
+}
+
+function draw(){
+background('#00FFFF');
+document.getElementById("font_size").innerHTML = "The font size will be = " + difference + "px";
+textSize(difference);
+fill('#000000');
+text('Aanvi',50,400);
+}
